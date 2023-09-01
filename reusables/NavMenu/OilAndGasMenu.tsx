c97d10119af0815723d89e7d66b547aa76dbf83d
Sebastianus Bara Primananda <sebastianus.bara@dana.id>
2023-09-01 13:26:34 +0700

    fix menu

--- reusables/NavMenu/OilAndGasMenu.tsx
+++ reusables/NavMenu/OilAndGasMenu.tsx
@@ -226,7 +226,7 @@ export const TrainingSubMenu = () => {
                         lineHeight: "22px",
                     }}
                 >
-                    Exploration
+                    Latest Training
                 </Text>
             </Box>
 
