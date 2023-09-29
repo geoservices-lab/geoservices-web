99d556abf233b9e3e235fae6ad8dc0dfb022554a
Muhamad Ridwan Firdaus <91183199+caballus40@users.noreply.github.com>
2023-09-29 14:21:23 +0700

    Update index.tsx
    add header in geothermal-demo

--- pages/geothermal-demo/index.tsx
+++ pages/geothermal-demo/index.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import Banner from "./component/banner"
+import Header from "module/BasicLayout/Header"
 
 const Geothermal = () => {
     return (
         <>
+        <Header/>
         <Banner/>
         </>
     )
