0233344c7d4b2093c13dda6e29c905b49f7102be
Sebastianus Bara Primananda <sebastianus.bara@dana.id>
2023-10-02 14:51:19 +0700

    integrate geothermal

--- pages/geothermal/index.tsx
+++ pages/geothermal/index.tsx
@@ -1,24 +1,42 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Banner from "./component/banner"
 import Header from "module/BasicLayout/Header"
 import BreadCrumb from "./component/breadcrumb"
-import Highlight from "./component/highlight"
 import Product from "./component/card"
 import Speciality from "./component/speciality"
 import Services from "./component/services"
 import Footer from "module/BasicLayout/Footer"
 import Equipments from "./component/equipments"
+import ImageJumbotron from "../../reusables/ImageJumbotron";
 
 const Geothermal = () => {
+    const [pageData, setPageData] = useState();
+
+    const callPageApi = async () => {
+        try {
+            const res = await fetch(`https://miib670e.api.sanity.io/v2021-06-07/data/query/production?query=*[_type == "geothermal"]`);
+            const data = await res.json();
+            setPageData(data.result[0]);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        callPageApi();
+    }, []);
+
+    console.log(pageData);
+
     return (
         <>
         <Header/>
-        <Banner/>
+        <ImageJumbotron imageSrc={pageData && pageData.image} text={pageData && pageData.title} />
         <div className='max-w-7xl mx-auto px-4 space-y-8'>
             <div className='mobile:hidden'>
                 <BreadCrumb/>
             </div>
-            <Product/>
+            <Product description={pageData && pageData.description}/>
             <div className="pt-14">
                 <Speciality/>
             </div>
