5897c794188e1b43e684175b142f56341620cb78
Sebastianus Bara Primananda <sebastianus.bara@dana.id>
2023-10-09 12:26:27 +0700

    fix news detail screen

--- pages/news/component/banner.tsx
+++ pages/news/component/banner.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-const Banner = () => {
+const Banner = ({ title }: any) => {
     return (
         <>
-        <div 
+        <div
             style={{
             backgroundImage: "url('https://source.unsplash.com/FXJf8y_cZA0')"
             }} className="desktop:h-[391px] h-[158px] w-full object-fill bg-cover grayscale">
                 <div className="desktop:w-full w-3/4 mx-auto py-16 desktop:py-32 max-w-7xl">
                     <h1 className="font-extrabold text-white text-[40px] desktop:text-[70px] capitalize leading-none">
-                    Newsroom
+                        {title}
                     </h1>
                 </div>
         </div>
