d9254cffa25204c53bf28a119dd61d177dfb2bfc
Sebastianus Bara Primananda <sebastianus.bara@dana.id>
2023-10-16 16:27:25 +0700

    fix data for trade and services

--- pages/geothermal/component/card.tsx
+++ pages/geothermal/component/card.tsx
@@ -5,7 +5,9 @@ const Product = ({ description }: any) => {
     return (
         <>
             <div className="border-t-4 border-peach shadow-md p-11">
-                <h1 className=' text-peach font-bold text-[24px] capitalize leading-6'>General information</h1>
+                <h1 className=' text-peach font-bold text-[24px] capitalize leading-6'>
+                    OVERVIEW
+                </h1>
                 <div className='desktop:text-left text-justify text-gray text-[16px] mt-4'>
                     <PortableText value={description} />
                 </div>
